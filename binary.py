d736a89f5c61e9b0ffa29c8a72265dd2f4dfec84
Fawaz Khan R <115716567+Fawaz-Khan-R@users.noreply.github.com>
2022-11-05 11:13:39 +0530

    Update binary.py

--- binary.py
+++ binary.py
@@ -1,9 +1,13 @@
+#This whole program put together counts the number of 1's in the binary form of a base 10 decimal.
+
+#This part of the program converts a base 10 decimal to binary.
 def binary(dcm):
     if dcm == 0:
         return 0
     else:
         return dcm % 2 + 10 * binary(int(dcm / 2))
 
+#This part of the program counts the number of 1's in any number.
 def ones(num):
     string = str(num)
     one = 0
@@ -16,7 +20,7 @@ def ones(num):
         if string[i] == '0':
             one = 0
     return mx
-        
+#Main program which uses the called functions.
 if __name__ == '__main__':
     n = int(input().strip())
     
